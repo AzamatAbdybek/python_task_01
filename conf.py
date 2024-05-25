634a8f5219ee3a42548391d5ce3f7a87a0502721
Azamat Abdybekov <azamat_abdybekov@epam.com>
2024-05-25 14:40:12 +0000

    fix_3

--- conf.py
+++ conf.py
@@ -1,22 +1,14 @@
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
-try:
-    # Load data from YAML file
-    with open('data.yml') as f:
-        data = yaml.safe_load(f)
-except Exception as e:
-    print(f"Error loading data.yml: {e}")
-    exit(1)
+# Load data from YAML file
+with open('data.yml') as f:
+    data = yaml.safe_load(f)
 
-try:
-    # Load Jinja2 template
-    env = Environment(loader=FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
-    template = env.get_template('vhosts.j2')
+# Load Jinja2 template
+env = Environment(loader=FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
+template = env.get_template('vhosts.j2')
 
-    # Render template using data and write to vhosts.conf
-    with open('vhosts.conf', 'w') as f:
-        f.write(template.render(data))
-except Exception as e:
-    print(f"Error rendering template or writing to file: {e}")
-    exit(1)
+# Render template using data and write to vhosts.conf
+with open('generaged_vhosts.conf', 'w') as f:
+    f.write(template.render(data))
