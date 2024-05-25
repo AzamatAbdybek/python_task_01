3df7e5d2532726f9d48e27faaaa75c1b4b2875c2
Azamat Abdybekov <azamat_abdybekov@epam.com>
2024-05-25 15:44:40 +0000

    fix_6

--- conf.py
+++ conf.py
@@ -1,26 +1,43 @@
 import yaml
 from jinja2 import Environment, FileSystemLoader
+import sys
+import os
 
-# Load data from YAML file
-try:
-    with open('data.yml') as f:
-        data = yaml.safe_load(f)
-except Exception as err:
-    print('Error while loading data from data.yml:', err)
-    exit(1)
+def main():
+    try:
+        # Check if data.yml exists and is readable
+        if not os.path.isfile('data.yml'):
+            sys.exit(1)
+        if not os.access('data.yml', os.R_OK):
+            sys.exit(1)
 
-# Load Jinja2 template
-env = Environment(loader=FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
-try:
-    template = env.get_template('vhosts.j2')
-except Exception as err:
-    print('Error while loading the vhosts.j2 template:', err)
-    exit(1)
+        # Check if vhosts.j2 exists and is readable
+        if not os.path.isfile('vhosts.j2'):
+            sys.exit(1)
+        if not os.access('vhosts.j2', os.R_OK):
+            sys.exit(1)
 
-# Render template using data and write to vhosts.conf
-try:
-    with open('generated_vhosts.conf', 'w') as f:
-        f.write(template.render(data))
-except Exception as err:
-    print('Error occurred while rendering the template or writing to generated_vhosts.conf:', err)
-    exit(1)
+        # Load the YAML data
+        with open('data.yml', 'r') as f:
+            data = yaml.safe_load(f)
+
+        # Setup Jinja2 environment
+        env = Environment(loader=FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
+        template = env.get_template('vhosts.j2')
+
+        # Render template using data and write to vhosts.conf
+        rendered_conf = template.render(vhosts=data['vhosts'])
+        with open('vhosts.conf', 'w') as f:
+            f.write(rendered_conf)
+
+        # Verify the output file is written and readable
+        if not os.path.isfile('vhosts.conf') or not os.access('vhosts.conf', os.R_OK):
+            sys.exit(1)
+
+    except yaml.YAMLError:
+        sys.exit(1)
+    except Exception:
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
