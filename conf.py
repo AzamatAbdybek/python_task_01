ae5f15fe1571588d6e6c47501fbd3788268214a5
Azamat Abdybekov <azamat_abdybekov@epam.com>
2024-05-25 15:29:10 +0000

    fix_5

--- conf.py
+++ conf.py
@@ -1,26 +1,26 @@
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
+# Load data from YAML file
 try:
-    # Load data from YAML file
     with open('data.yml') as f:
         data = yaml.safe_load(f)
-except Exception as error:
-    print('Failed to load data.yml:', error)
+except Exception as err:
+    print('Error while loading data from data.yml:', err)
     exit(1)
 
+# Load Jinja2 template
+env = Environment(loader=FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
 try:
-    # Load Jinja2 template
-    env = Environment(loader=FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
     template = env.get_template('vhosts.j2')
-except Exception as error:
-    print('Failed to load vhosts.j2 template:', error)
+except Exception as err:
+    print('Error while loading the vhosts.j2 template:', err)
     exit(1)
 
+# Render template using data and write to vhosts.conf
 try:
-    # Render template using data and write to vhosts.conf
     with open('generated_vhosts.conf', 'w') as f:
         f.write(template.render(data))
-except Exception as error:
-    print('Failed to render template or write to generated_vhosts.conf:', error)
+except Exception as err:
+    print('Error occurred while rendering the template or writing to generated_vhosts.conf:', err)
     exit(1)
